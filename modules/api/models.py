32e483d157516f9fb8f19869be7319f360abf936
DrissABXR <117480396+DrissABXR@users.noreply.github.com>
2024-02-12 17:40:33 +0100

    Update models.py

--- modules/api/models.py
+++ modules/api/models.py
@@ -1,5 +1,5 @@
 import inspect
-
+from typing import Dict, List
 from pydantic import BaseModel, Field, create_model
 from typing import Any, Optional, Literal
 from inflection import underscore
@@ -188,6 +188,7 @@ class ProgressResponse(BaseModel):
     state: dict = Field(title="State", description="The current state snapshot")
     current_image: str = Field(default=None, title="Current image", description="The current image in base64 format. opts.show_progress_every_n_steps is required for this to work.")
     textinfo: str = Field(default=None, title="Info text", description="Info text used by WebUI.")
+    current_processed_images: List[str] = Field(default=None, title="Images", description="The generated images in base64 format.")
 
 class InterrogateRequest(BaseModel):
     image: str = Field(default="", title="Image", description="Image to work on, must be a Base64 string containing the image's data.")
